122bd1c06e38c0525c3eb62a819cbd335aebb918
KiRa <cresciucmax@gmail.com>
2023-11-26 16:42:44 +0200

    main

--- jwt/src/main/java/ks/msx/jwt/controller/TestController.java
+++ jwt/src/main/java/ks/msx/jwt/controller/TestController.java
@@ -1,5 +1,6 @@
 package ks.msx.jwt.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import ks.msx.jwt.utility.JwtUtility;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,21 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.NoSuchAlgorithmException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 @RestController
 @AllArgsConstructor
 public class TestController {
     private final JwtUtility jwtUtility;
 
     @GetMapping("/test/endpoint/generate")
-    public ResponseEntity<?> generateTokenEndpoint(){
+    public ResponseEntity<?> generateTokenEndpoint(HttpServletResponse response) throws NoSuchAlgorithmException {
         String token = jwtUtility.generateToken("k");
+        response.addHeader(AUTHORIZATION, "Bearer " + token);
         return new ResponseEntity<>(token, HttpStatus.OK);
     }
 
     @GetMapping("/test/endpoint/verify")
     public ResponseEntity<?> verifyToken(@RequestParam(name = "token")String token){
         String ExpectedUsername = "k";
-        String currentUsername = jwtUtility.decodeToken(token).getSubject();
+        String currentUsername = jwtUtility.getUsernameFromToken(token);
         return new ResponseEntity<>(ExpectedUsername.equals(currentUsername), HttpStatus.OK);
     }
 }
--- jwt/src/main/java/ks/msx/jwt/utility/JwtUtility.java
+++ jwt/src/main/java/ks/msx/jwt/utility/JwtUtility.java
@@ -1,38 +1,43 @@
 package ks.msx.jwt.utility;
 
-import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 public class JwtUtility {
     private final static Logger logger = LoggerFactory.getLogger(JwtUtility.class);
-    private final static String key = "javaKeyProject";
-    private final static long jwtExpirationMs = 2000L;
+    private final static String key = "javaLangTestJwt20RE21Specs=JRUWTaskKREFGH19";
+    private final static long jwtExpirationMs = 20000000000000L;
 
 
-    public String generateToken(String username){
+    public String generateToken(String username) throws NoSuchAlgorithmException {
+        String token = "";
         try {
-            return Jwts.builder()
+            token = Jwts.builder()
                     .setSubject(username)
+                    .claim("name", username)
                     .setIssuedAt(new Date())
                     .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
-                    .signWith(SignatureAlgorithm.HS512, key)
+                    .signWith(SignatureAlgorithm.HS256, key)
                     .compact();
         }catch (Exception e){
             logger.error(String.valueOf(e));
         }
-        return null;
+        return token;
     }
 
-    public Claims decodeToken(String token){
-        return Jwts.parser().build().parseSignedClaims(token).getPayload();
+    public String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+
+        return claims.getSubject();
     }
 }
